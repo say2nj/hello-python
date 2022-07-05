600ab48cb1553edcc1379f26ae041f0e83a01e2a
Nikhil Jha <say2nikhiljha@gmail.com>
2022-07-05 16:55:55 +0530

    Completed Day 6

---
+++ Day 6/function-check.py
@@ -0,0 +1,13 @@
+# Functions
+
+# -------Built-in Functions ---------
+print("Hello")
+num_char = len("Hello")
+print(num_char)
+
+# ------- User-defined functions ------
+def myFunction():
+    print("Hello")
+    print("Bye")
+
+myFunction()
